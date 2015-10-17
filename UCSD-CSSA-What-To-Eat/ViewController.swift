3495f259efc086a754978334e2f649255af3fa84
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-10-17 12:16:04 -0700

    First commit

--- UCSD-CSSA-What-To-Eat/ViewController.swift
+++ UCSD-CSSA-What-To-Eat/ViewController.swift
@@ -4,22 +4,39 @@
 //
 //  Created by Ruiqing Qiu on 10/17/15.
 //  Copyright © 2015 Ruiqing Qiu. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    var shaked = true
+    //@IBOutlet weak var shakeLabel: UILabel!
+    @IBOutlet var shakeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            shaked = !shaked;
+            if(!shaked){
+                self.shakeLabel.text = "╯' - ')╯︵ ┻━┻"
+            }
+            else{
+                self.shakeLabel.text = " ┬─┬ ノ( ' - 'ノ)"
+                
+            }
+        }
+    }
 }
 
