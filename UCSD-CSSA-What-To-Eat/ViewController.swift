6a71b932cb59aa1eff8ff86f6b7b329a9167d908
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-10-17 12:06:41 -0700

    Initial Commit

---
+++ UCSD-CSSA-What-To-Eat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCSD-CSSA-What-To-Eat
+//
+//  Created by Ruiqing Qiu on 10/17/15.
+//  Copyright © 2015 Ruiqing Qiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UCSD-CSSA-What-To-EatTests/UCSD_CSSA_What_To_EatTests.swift
@@ -0,0 +1,36 @@
+//
+//  UCSD_CSSA_What_To_EatTests.swift
+//  UCSD-CSSA-What-To-EatTests
+//
+//  Created by Ruiqing Qiu on 10/17/15.
+//  Copyright © 2015 Ruiqing Qiu. All rights reserved.
+//
+
+import XCTest
+@testable import UCSD_CSSA_What_To_Eat
+
+class UCSD_CSSA_What_To_EatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
