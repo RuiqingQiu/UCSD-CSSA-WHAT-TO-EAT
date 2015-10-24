0f07201b7ae8f563358a12a2fd25bb8ddad3d7ba
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-10-24 11:56:28 -0700

    Image added

--- UCSD-CSSA-What-To-Eat/ViewController.swift
+++ UCSD-CSSA-What-To-Eat/ViewController.swift
@@ -12,14 +12,34 @@ class ViewController: UIViewController {
     var shaked = true
 
     //@IBOutlet weak var shakeLabel: UILabel!
+    //For displaying "shake me"
     @IBOutlet var shakeLabel: UILabel!
     
+    //For displaying the result resturant name
     @IBOutlet weak var button: UIButton!
-    
+    //For displaying the result resturant image
+
+    @IBOutlet var Rest_Image: UIButton!
     var i = Int(arc4random_uniform(10))
     
-    var nameArray = ["Shogun", "Panda", "Tapioca", "Bombay Coast","Pines","Santorini","Lemon Grass","Shogun","Shogun","Shogun"]
-
+    var pc_rest = ["Shogun", "Panda", "Tapioca", "Bombay Coast","Lemon Grass","Subway","Santorini","Burger King","Jumba Juice","Dlush"]
+    
+    var other_rest = ["Perks", "Art of Espresso Café", "Bella Vista Social Club and Caffé",
+        "Short Stop",
+        "HomePlate",
+        "Peets Coffee and Tea",
+        "Hi Thai",
+        "64 Degrees",
+        "Sixty-Four North",
+        "Cafe Ventanas",
+        "Canyon Vista",
+        "Foodworx",
+        "Pines",
+        "Goodys Coffee Window",
+        "Goodys",
+        "Roots",
+        "The Bistro at The Strand"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -46,9 +66,10 @@ class ViewController: UIViewController {
         if motion == .MotionShake {
             
             
-            i = Int(arc4random_uniform(10))
-            button.setTitle(nameArray[i], forState: .Normal)
-        
+            i = Int(arc4random_uniform(UInt32(pc_rest.count)))
+            button.setTitle(pc_rest[i], forState: .Normal)
+            Rest_Image.setImage(UIImage(named: pc_rest[i] + ".png"), forState: UIControlState.Normal)
+
         }
 
     }
