5fc33fcef082a4a23f7abceefc05bd92412d7bc3
ruiqingqiu <raymond.qiu1993@gmail.com>
2016-02-06 15:53:24 -0800

    setting page added

---
+++ UCSD-CSSA-What-To-Eat/SettingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingViewController.swift
+//  UCSD-CSSA-What-To-Eat
+//
+//  Created by Ruiqing Qiu on 2/6/16.
+//  Copyright © 2016 Ruiqing Qiu. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UITableViewController {
+    
+    @IBOutlet var List1: UITextField!
+    @IBOutlet var List2: UITextField!
+    @IBOutlet var List3: UITextField!
+    @IBOutlet var List4: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        List1.addTarget(self, action: "List1TextDidChange:", forControlEvents: UIControlEvents.EditingChanged)
+        List2.addTarget(self, action: "List2TextDidChange:", forControlEvents: UIControlEvents.EditingChanged)
+        List3.addTarget(self, action: "List3TextDidChange:", forControlEvents: UIControlEvents.EditingChanged)
+        List4.addTarget(self, action: "List4TextDidChange:", forControlEvents: UIControlEvents.EditingChanged)
+    }
+
+    func List1TextDidChange(textField: UITextField) {
+        print("List 1 text changed to " + textField.text!);
+    }
+    func List2TextDidChange(textField: UITextField) {
+        print("List 2 text changed to " + textField.text!);
+    }
+    func List3TextDidChange(textField: UITextField) {
+        print("List 3 text changed to " + textField.text!);
+    }
+    func List4TextDidChange(textField: UITextField) {
+        print("List 4 text changed to " + textField.text!);
+    }
+}
